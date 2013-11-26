2e2887b0663aac7fc59046543ddf5900c22f09c0
Denis Spiridonov <sdi@rshu.ru>
2013-11-26 16:42:13 +0400

    added time variable to test

--- src/pydap/handlers/binary/ssmi/__init__.py
+++ src/pydap/handlers/binary/ssmi/__init__.py
@@ -287,12 +287,13 @@ class BinarySsmiHandler(BaseHandler):
 def _test():
 
     test_values = {}
+    test_values['time'] = [7.10, 7.10, 7.10, 7.10, 7.10]
     test_values['wspd'] = [253.00, 253.00, 6.40, 5.80, 5.60]
     test_values['vapor'] = [253.00, 253.00, 60.60, 60.90, 60.30]
     test_values['cloud'] = [253.00, 253.00, 0.07, 0.08, 0.06]
     test_values['rain'] = [253.00, 253.00, 0.00, 0.00, 0.00]
 
-    values = ['wspd', 'vapor', 'cloud', 'rain']
+    values = ['time', 'wspd', 'vapor', 'cloud', 'rain']
 
     application = BinarySsmiHandler("../../../../../test/f10_19950120v7.gz")
     environ = {}
@@ -301,7 +302,9 @@ def _test():
         environ['QUERY_STRING'] = value+"[171:1:171][273:1:277][0]"
         dd = application.parse_constraints(environ)
 
-        scale_factor = dd[value].attributes['scale_factor']
+        # time scale factors 6.0 (for observation minute) and 0.1 (for observation hour)
+        # test using observation hour
+        scale_factor = dd[value].attributes['scale_factor'] if value != 'time' else 0.1
         add_offset = dd[value].attributes['add_offset']
 
         _value_arr = dd[value][value][:]
@@ -310,15 +313,18 @@ def _test():
         for j in range(_shape[1]):
             for i in range(_shape[0]):
                 for k in range(_shape[2]):
-                    if _value_arr[i][j][k]> 250:
-                        if _value_arr[i][j][k] != test_values[value][j]:
+                    _value = _value_arr[i][j][k]
+                    if _value > 250:
+                        print _value, test_values[value][j]
+                        if _value != test_values[value][j]:
                             print value, ": failed"
                             return 1
                     else:
-                        if round((_value_arr[i][j][k]*scale_factor + add_offset)*1000)/1000.0 != test_values[value][j]:
+                        print round((_value*scale_factor + add_offset)*1000)/1000.0, test_values[value][j]
+                        if round((_value*scale_factor + add_offset)*1000)/1000.0 != test_values[value][j]:
                             print value, ": failed"
                             return 1
-        print value, ": ok"
+        print value, ": ok\n"
     print "OK (All %d tests)" %len(values)
 
 if __name__ == "__main__":
